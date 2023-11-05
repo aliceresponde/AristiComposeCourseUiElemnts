c37b9bebe25e23a3aef24c49d7c43668cadc15c4
aliceLit <Alicia.Beltran@litmos.com>
2023-11-04 22:44:13 -0500

    MyText

--- app/src/main/java/com/aliceresponde/aristicomposecourse/MainActivity.kt
+++ app/src/main/java/com/aliceresponde/aristicomposecourse/MainActivity.kt
@@ -11,15 +11,21 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -46,14 +52,80 @@ class MainActivity : ComponentActivity() {
             AristiComposeCourseTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    MyTextSample()
+
+                    var myText by rememberSaveable { mutableStateOf("") }
+
+                    Column(
+                        Modifier
+                            .fillMaxSize()
+                            .padding(16.dp)
+                    ) {
+                        MyStatelessOutlinedTextField(text = myText, textLabel = "Enter your name", onValueChange = {
+                            myText = if (it.contains("a")) it.replace("a", "") else it
+                        })
+
+
+                        Text(text = "Hello World!")
+                    }
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyStatelessOutlinedTextField(text: String, textLabel: String, onValueChange: (String) -> Unit) {
+    OutlinedTextField(
+        value = text,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+            .horizontalScroll(rememberScrollState())
+            .verticalScroll(rememberScrollState()),
+        onValueChange = { onValueChange(it) },
+        label = { Text(text = textLabel) },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.Magenta,
+            unfocusedBorderColor = Color.Blue
+        )
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun MyOutlinedTextField() {
+    var myText by rememberSaveable { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = myText,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+            .horizontalScroll(rememberScrollState())
+            .verticalScroll(rememberScrollState()),
+        onValueChange = { myText = if (it.contains("a")) it.replace("a", "") else it },
+        label = { Text(text = "Enter your name") },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.Magenta,
+            unfocusedBorderColor = Color.Blue
+        )
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun MyTextField() {
+    var myText by rememberSaveable { mutableStateOf("") }
+
+    TextField(
+        value = myText,
+        onValueChange = { myText = if (it.contains("a")) it.replace("a", "") else it },
+        label = { Text(text = "Enter your name") }
+    )
+}
+
 @Composable
 fun MyTextSample() {
     Column(
@@ -63,10 +135,12 @@ fun MyTextSample() {
         Text(text = "Hello World!")
         Text(text = "Hello World!", color = Color.Red)
         Text(text = "Hello World!", Modifier.size(100.dp, 100.dp))
-        Text(text = "Hello World!",
+        Text(
+            text = "Hello World!",
             Modifier
                 .size(100.dp, 100.dp)
-                .background(Color.Red))
+                .background(Color.Red)
+        )
         Text(text = "Hello World!", fontSize = 20.sp)
         Text(text = "Hello World!", fontFamily = FontFamily.Cursive)
         Text(text = "Hello World!", fontWeight = androidx.compose.ui.text.font.FontWeight.Bold)
